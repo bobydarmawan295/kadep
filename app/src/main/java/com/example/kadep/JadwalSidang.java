ad82cc49874d5182c25fbbd9882c7e7a0370e6a7
Boby Darmawan <2011522023@student.unand.ac.id>
2022-09-22 15:27:33 +0700

    update

---
+++ app/src/main/java/com/example/kadep/JadwalSidang.java
@@ -0,0 +1,14 @@
+package com.example.kadep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JadwalSidang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jadwal_sidang);
+    }
+}
--- app/src/main/java/com/example/kadep/MainActivity.java
+++ app/src/main/java/com/example/kadep/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.kadep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.time.chrono.JapaneseDate;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button jadwalButton;
+    Button permintaanTa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jadwalButton = findViewById(R.id.jadwal);
+        jadwalButton.setOnClickListener(view -> {
+            Intent loggedIn = new Intent(MainActivity.this, JadwalSeminar.class);
+            startActivity(loggedIn);
+        });
+
+        permintaanTa = findViewById(R.id.permintaan);
+        permintaanTa.setOnClickListener(view -> {
+            Intent RequestTA = new Intent(MainActivity.this, PermintaanTA.class);
+            startActivity(RequestTA);
+        });
     }
 }
