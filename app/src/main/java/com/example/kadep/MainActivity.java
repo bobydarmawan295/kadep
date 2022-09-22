f3aa5eadde0f4a2226a5668810ae5e46dc5ee2fe
Boby Darmawan <2011522023@student.unand.ac.id>
2022-09-22 22:11:37 +0700

    update

--- app/src/main/java/com/example/kadep/MainActivity.java
+++ app/src/main/java/com/example/kadep/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.kadep.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
     private boolean isLoggedIn = false;
+    Button jadwalButton, mintaSidangButton, jadwalSeminar, mintaSeminar, mahasiswaTA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +35,30 @@ public class MainActivity extends AppCompatActivity {
 
         binding.nama.setText(username);
 
+
+        jadwalButton = findViewById(R.id.jadwalSidang);
+        jadwalButton.setOnClickListener(views -> {
+            Intent jadwalSidang = new Intent(MainActivity.this, JadwalSidang.class);
+            startActivity(jadwalSidang);
+        });
+
+        mintaSidangButton = findViewById(R.id.mintaSidang);
+        mintaSidangButton.setOnClickListener(views -> {
+            Intent mintaSidang = new Intent(MainActivity.this, PermintaanSidang.class);
+            startActivity(mintaSidang);
+        });
+
+        jadwalSeminar = findViewById(R.id.button4);
+        jadwalSeminar.setOnClickListener(views -> {
+            Intent mintaSeminar = new Intent(MainActivity.this, JadwalSeminar.class);
+            startActivity(mintaSeminar);
+        });
+
+        mintaSeminar = findViewById(R.id.button5);
+        mintaSeminar.setOnClickListener(views -> {
+            Intent mintaSeminar = new Intent(MainActivity.this, PermintaanSeminar.class);
+            startActivity(mintaSeminar);
+        });
+        
     }
 }
